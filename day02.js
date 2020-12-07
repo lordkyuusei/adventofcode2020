7154e561fd076055e089175272478a2beb16aeb4
Kyuusei <kevin.poirot@epitech.eu>
2020-12-07 15:29:07 +0100

    Update day02.js

--- day02.js
+++ day02.js
@@ -1,31 +1,40 @@
 const puzzle = [
-  "15-16 m: mhmjmzrmmlmmmmmm",
-  "5-6 d: dcdddhzld",
-  "3-4 s: vqssdcbl",
-  "0-0 y: OfCourseThereIsMoreThanSixPasswordsButICannotMakethisTooLong",
-  "9-12 t: tvwstttttttktwt",
-  "4-6 g: vslqbgg",
-  "9-16 d: dsdddddddrdddddhdbdd",
+	"1-3 a: abcde",
+	"1-3 b: cdefg",
+	"2-9 c: ccccccccc",
 ];
 
-let valid = 0;
+let valid1 = 0;
+let valid2 = 0;
+
+const part_one = (password, letter, min, max) => {
+  const count = (password.match(new RegExp(letter, 'g') || [])).length;
+  if (count >= min && count <= max) {
+    valid1 += 1;
+  }
+}
+
+const part_two = (password, letter, fpos, spos) => {
+	const policy = password[fpos-1] === letter ^ password[spos-1] === letter;
+  if (policy) {
+  	valid2 += 1;
+  }
+}
+
 puzzle.forEach(line => {
 	const context = line.split(" ");
   const letter = context[1].slice(0, -1);
   const password = context[2];
 
 	const range = context[0].split("-");
-  const [min, max] = [range[0], range[1]];
-  console.log(`We need to find ${letter} between ${min} and ${max} times in ${password}`);
+  const [nb1, nb2] = [range[0], range[1]];
   try {
-    const count = (password.match(new RegExp(letter, 'g') || [])).length;
-    if (count >= min && count <= max) {
-      console.log("It respects the policy!");
-      valid += 1;
-    }
+  	part_one(password, letter, nb1, nb2);
+    part_two(password, letter, nb1, nb2);
   } catch(error) {
-  	console.error(error);
+  	return;
   }
-})
+});
 
-console.log(valid);
+console.log(valid1);
+console.log(valid2);
