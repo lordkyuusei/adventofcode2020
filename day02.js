adc0585d17dbfc39e26c9b94d1b464abb6b430a0
Kyuusei <kevin.poirot@epitech.eu>
2020-12-07 15:33:15 +0100

    Update day02.js

--- day02.js
+++ day02.js
@@ -8,32 +8,31 @@ let valid1 = 0;
 let valid2 = 0;
 
 const part_one = (password, letter, min, max) => {
-  const count = (password.match(new RegExp(letter, 'g') || [])).length;
-  if (count >= min && count <= max) {
-    valid1 += 1;
-  }
+	const count = (password.match(new RegExp(letter, 'g') || [])).length;
+	if (count >= min && count <= max) {
+    	valid1 += 1;
+  	}
 }
 
 const part_two = (password, letter, fpos, spos) => {
 	const policy = password[fpos-1] === letter ^ password[spos-1] === letter;
-  if (policy) {
-  	valid2 += 1;
-  }
+  	if (policy) {
+  		valid2 += 1;
+  	}
 }
 
 puzzle.forEach(line => {
 	const context = line.split(" ");
-  const letter = context[1].slice(0, -1);
-  const password = context[2];
-
+	const letter = context[1].slice(0, -1);
+	const password = context[2];
 	const range = context[0].split("-");
-  const [nb1, nb2] = [range[0], range[1]];
-  try {
-  	part_one(password, letter, nb1, nb2);
-    part_two(password, letter, nb1, nb2);
-  } catch(error) {
-  	return;
-  }
+	const [nb1, nb2] = [range[0], range[1]];
+	try {
+  		part_one(password, letter, nb1, nb2);
+    	part_two(password, letter, nb1, nb2);
+  	} catch(error) {
+  		return;
+  	}
 });
 
 console.log(valid1);
